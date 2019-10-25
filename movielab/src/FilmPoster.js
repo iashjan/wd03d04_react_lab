7d3c4b96d4f7ba0f9d60fae58829bd227a3dfb0b
iashjan <ashjanot@gmail.com>
2019-10-25 20:29:10 +0300

    lab done

---
+++ movielab/src/FilmPoster.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FilmPoster = (props) => {
+    return (
+    
+             <div className="film-row">
+  <img src={props.poster} alt="" />
+        </div>
+    )
+}
+
+export default FilmPoster
