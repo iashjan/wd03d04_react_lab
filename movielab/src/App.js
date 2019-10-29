51bd1a77e842f8ef8642abed741ccfea2b6bde6f
iashjan <ashjanot@gmail.com>
2019-10-30 01:44:25 +0300

    part3

--- movielab/src/App.js
+++ movielab/src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
-import FilmListing from './FilmListing';
+import FilmDetails from './FilmDetails.js';
+import FilmListing from './FilmListing.js';
+import TMDB from './TMDB.js';
 
-function App() {
-  return (
-    <div className="film-library">
-      
-     
-    <FilmListing />
+class App extends Component {
+  
+    state = {
+        films :  TMDB.films,
+        faves : [],
+        current: [],
+    }
     
+    handleFaveToggle(films){
+        const faves = faves.setstate.slice();
+        const filmIndex = Array.prototype.indexOf(0) ;
+       
+    }
 
-  <div className="film-details">
-    <h1 className="section-title">DETAILS</h1>
-    
-  </div> 
-</div>
-  );
+    render() {
+        return (
+            <div className="App" >
+            <div className="film-library">
+              <FilmListing films={TMDB.films} />
+              <FilmDetails films={TMDB.films} />
+              {this.handleFaveToggle = this.handleFaveToggle.bind(this)}
+            </div>
+          </div>
+        );
+    }
 }
-
 export default App;
--- movielab/src/Fav.js
+++ movielab/src/Fav.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 export default class fav extends Component {
     constructor(props){
         super(props)
@@ -8,12 +7,21 @@ export default class fav extends Component {
         }
     }
     handleClick =(e) => {
-        console.log("handling Fave click!");
-        this.setState({isFave: !this.state.isFave})
-       e.stopPropagation()
-    }
+        e.stopPropagation()
+  console.log('Handling Fave click!')
+  this.props.onFaveToggle()
+
+}
+onFaveToggle (){
+
+}
+    //     console.log("handling Fave click!");
+    //     this.setState({isFave: !this.state.isFave})
+    //    e.stopPropagation()
+   
     render() {
-        const isFave = (this.state.isFave) ? 'remove_from_queue' : 'add_to_queue'
+        const isFave = (this.props.isFave) ?'remove_from_queue' : 'add_to_queue'
+     
         return (
             <div className={"film-row-fave "+isFave}onClick={this.handleClick}>
   <p className="material-icons">{isFave}</p>
